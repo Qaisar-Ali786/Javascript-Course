e82b6f342526ef443503357b9b142d3a7c452454
Qaisar-Ali786 <qaisarcaptain9@gmail.com>
2025-12-17 08:56:11 -0800

    Basics

---
+++ part01/app.js
@@ -0,0 +1,56 @@
+// null can be set by programmer
+// undefined is set by javascript compiler
+// let notDefinied;
+// if you dont want to assign value to this notDefiner than it is also consider undefined by javascript compiler
+
+//  relational operator
+
+// 1--comparison operator
+
+// greater than >;
+// less than <;
+// greater or equal >=;
+// less or equal <=
+// equal operator =;
+
+// 2--equality operator
+
+// === strict equlaity (type + value)
+// console.log(10 === `10`);
+// === strict non-equality operators (type + value)
+// console.log(10 !== `10`);
+// lose equality operator (value)
+// console.log(10 == `10`);
+// =/ not equality operator (value)
+
+// all about string
+
+// 1 concation operator
+
+// let firstNam = `qaisar `;
+// let secondName = `ali`;
+// let fullName = firstNam.concat(secondName);
+// console.log(fullName);
+// let firstNam = `qaisar`;//
+
+// 2 append method;
+
+// let firstNam = `qaisar`;
+// firstNam += `how are you`;
+// console.log(firstNam);
+
+// 4 cases;
+
+// console.log(firstNam.toUpperCase());
+
+// 5 slices
+// console.log(firstNam.slice(1, 3));
+
+//  6 split & joint
+// console.log(firstNam.split(" ").join(""));
+
+// 7 includes
+// console.log(firstNam.includes("i"));
+
+// 8 trim
+// console.log(firstNam.trim());
