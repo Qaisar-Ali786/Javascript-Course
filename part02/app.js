ba875b007eb7cd727ebb8cfd4ec1ec9bb3446279
Qaisar-Ali786 <qaisarcaptain9@gmail.com>
2025-12-29 23:59:35 -0800

    part 2 added

---
+++ part02/app.js
@@ -0,0 +1,71 @@
+
+
+//type conversion in js
+// string into number
+// let cash = `12`;
+// cash = +cash;
+// cash = parseInt(cash);
+// cash = Number(cash);
+// console.log(cash);
+// console.log(typeof cash);
+
+// number into string
+// let number = 12;
+// number = number.toString();
+// number = String(number);
+// console.log(number);
+// console.log(typeof number);
+
+// convert string into decimal
+// let money = `34.098`;
+// money = parseFloat(money);
+// console.log(money);
+// console.log(typeof money);
+
+// conditional statement
+
+// let time = 20;
+// let greeting;
+// if (time < 20) {
+//   greeting = `hellow everyone`;
+//   console.log(greeting);
+// } else if (time > 10) {
+//   greeting = `goodbye`;
+//   console.log(greeting);
+// } else {
+//   greeting = `goodbyr everyone`;
+//   console.log(greeting);
+// }
+
+// switch statement
+
+// let x = 0;
+// let text;
+// switch (x) {
+//   case 0:
+//     buld = `off`;
+//     console.log(buld);
+//     break;
+//   case 1:
+//     buld = `on`;
+//     console.log(bulb);
+//     break;
+//   default:
+//     bulb = `no value found`;
+//     console.log(bulb);
+// }
+
+// let fruit = `potato`;
+// switch (fruit) {
+//   case `banana`:
+//     console.log(`banana is good`);
+//     break;
+//   case `orange`:
+//     console.log(`i am not a fan of orange`);
+//     break;
+//   case `apple`:
+//     console.log(`how you like them apples`);
+//     break;
+//   default:
+//     console.log(`i have never heard of that fruit`);
+// }
