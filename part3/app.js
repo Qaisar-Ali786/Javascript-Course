2258444a8a635a43f5ff7209b9e69c7d33ab370e
Qaisar-Ali786 <qaisarcaptain9@gmail.com>
2025-12-30 23:27:35 -0800

    Part2 added

---
+++ part3/app.js
@@ -0,0 +1,100 @@
+
+
+// for loops
+
+// let name = `qaisar`;
+// for (let i = 0; i <= 1000; i++) {
+//   console.log(name, i);
+// }
+
+// while loop
+
+// let i = 0;
+// while (i <= 100) {
+//   console.log(`hellow everyone`, i);
+//   i++;
+// }
+// let i = 10;
+// while (i <= 100) {
+//   console.log(`qaisar ali`, i);
+//   i++;
+// }
+
+// do while loop
+
+// let i = 10;
+// do {
+//   console.log(`qaisar`, i);
+//   i++;
+// } while (i <= 100);
+
+// logical operators
+
+// ---logical  AND (&&)
+// true if both operators value are true else evaluate to false
+// const a = true;
+// const b = true;
+// const c = 4;
+// console.log(a && b);
+
+// ---logical or (||)
+// true if either of the operator are true. evaluate to false if both are false
+// const a = true;
+// const b = false;
+// console.log(a || b);
+// --logical not (!)
+// true if the operands is false and vice versa
+// const a = true;
+// const b = false;
+// console.log(!b);
+
+// ex
+
+// let password = `qaisar12`;
+// if ((password.length = 12 || password.includes(`12`))) {
+//   console.log(`valid password`);
+// } else {
+//   console.log(`invlaid password`);
+// }
+
+// let isTrue = true;
+// console.log(!isTrue);
+
+// data structure
+//  number array
+// arras are data structure which allows to store multiple of data
+// const myList = [1, 2, 3, 4, 2];
+// console.log(myList);
+// stringarray
+// const stringArray = [`eat`, `sleep`, `code`, `repeat`];
+// console.log(stringArray);
+// accessing items from array
+// console.log(stringArray[1]);
+// console.log(stringArray[2]);
+// console.log(stringArray[0]);
+
+// nested array or two dimension array
+// const nestArr = [`one`, [`two`, [`qaisar`, `ali`], `three`, `four`]];
+// console.log(nestArr[1][1]);
+
+// array methods
+// const fruits = [`apples`, `banana`, `mango`];
+// fruits.push(`carry`);
+// console.log(fruits);
+// fruits.pop();
+// console.log(fruits);
+// fruits.shift();
+// console.log(fruits);
+// fruits.unshift(`apple`);
+// console.log(fruits);
+// const newFruits = [`strawberry`, `pinapple`];
+// const totalFruits = fruits.concat(newFruits);
+// console.log(totalFruits);
+// const pl = [`javascriptr`, `golang`, `python`, `php`];
+// console.log(pl.reverse());
+// console.log(pl.slice(0, 3));
+
+// console.log(pl.includes(`golang`));
+// console.log(pl.join(`-`));
+// const numbers = [2, 3, 4, 5, 7, 34, 23, 12, 3, 4, 5];
+// console.log(numbers.sort());
