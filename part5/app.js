211e17514eed307acd484f405859b58ae0a9135f
Qaisar-Ali786 <qaisarcaptain9@gmail.com>
2026-01-03 02:31:56 -0800

    part 5

---
+++ part5/app.js
@@ -0,0 +1,28 @@
+// method in js
+
+// const item = {
+//   price: 12,
+//   name: `osaka`,
+//   location: `mg`,
+//   greet: function () {
+//     return `hellow the product price is ${item.price} and the name of product is ${item.name} and the location of our site is ${item.location}`;
+//   },
+// };
+// console.log(item.greet());
+
+// JSON
+
+// const person = {
+//   name: "qaisar",
+//   age: 20,
+//   email: "@gmail.com",
+//   isSubscribed: true,
+//   hobbies: ["reading", "running", "cooking"],
+//   address: {
+//     city: "new York",
+//     idj: true,
+//   },
+// };
+// const jsonstring = JSON.stringify(person);
+// const parseObject = JSON.parse(jsonstring);
+// console.log(person);
