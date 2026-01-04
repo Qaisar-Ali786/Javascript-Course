c1aed49ea7bc08b33ba4d15d9dedec894d41e7bf
Qaisar-Ali786 <qaisarcaptain9@gmail.com>
2026-01-03 21:35:00 -0800

    Part 4

---
+++ part6/app.js
@@ -0,0 +1,40 @@
+// date
+
+// const currentDate = new Date(2025, 3, 24, 12, 34, 0, 0);
+// console.log(currentDate);
+
+// const date = new Date();
+// const year = date.getFullYear();
+// const month = date.getMonth();
+// const day = date.getDay();
+// console.log(`month , ${month}`);
+// console.log(`year , ${year}`);
+// console.log(`day , ${day}`);
+// const date = new Date();
+// console.log(date.toDateString());
+// console.log(date.toISOString());
+// console.log(date.toLocaleString());
+
+// date.setDate(date.getDate() + 1);
+// date.setDate(date.getMonth() + 1);
+// console.log(date);
+
+// setInterval
+
+// setInterval(
+//   () => console.log(`this function will take 3 second to execute`),
+//   3000
+// );
+
+// setTimeout(function () {
+//   console.log(`this function will be executes after 3 seconds`);
+// }, 3000);
+
+// const intervalId = setInterval(function () {
+//   console.log(`hy everyone`);
+// }, 1000);
+
+// setTimeout(function () {
+//   clearInterval(intervalId);
+//   console.log(`interval has been ended`);
+// }, 10000);
